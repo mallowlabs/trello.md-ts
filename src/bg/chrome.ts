c930ceb7b3ba29c99a159a6494af7b001a86fcfd
mallowlabs <mallowlabs@gmail.com>
2023-03-19 00:39:28 +0900

    Comment out chrome.ts

--- src/bg/chrome.ts
+++ src/bg/chrome.ts
@@ -1,29 +1,29 @@
-namespace Tabs {
-  export type tab = {
-    url: string | null;
-    id: number;
-  };
+// namespace Tabs {
+//   export type tab = {
+//     url: string | null;
+//     id: number;
+//   };
 
-  export namespace OnUpdated {
-    export function add_listener(
-      callback: (id: number) => (changeInfo: any) => (tab: tab) => void
-    ): void {
-      return chrome.tabs.onUpdated.addListener(callback);
-    }
-  }
-}
+//   export namespace OnUpdated {
+//     export function add_listener(
+//       callback: (id: number) => (changeInfo: any) => (tab: tab) => void
+//     ): void {
+//       return chrome.tabs.onUpdated.addListener(callback);
+//     }
+//   }
+// }
 
-namespace PageAction {
-  export function show(tabId: number): void {
-    return chrome.pageAction.show(tabId);
-  }
-  export function set_icon(details: { tabId: number; path: string }): void {
-    return chrome.pageAction.setIcon(details);
-  }
+// namespace PageAction {
+//   export function show(tabId: number): void {
+//     return chrome.pageAction.show(tabId);
+//   }
+//   export function set_icon(details: { tabId: number; path: string }): void {
+//     return chrome.pageAction.setIcon(details);
+//   }
 
-  export namespace OnClicked {
-    export function add_listener(callback: (tab: chrome.tabs.Tab) => void): void {
-      return chrome.pageAction.onClicked.addListener(callback);
-    }
-  }
-}
+//   export namespace OnClicked {
+//     export function add_listener(callback: (tab: chrome.tabs.Tab) => void): void {
+//       return chrome.pageAction.onClicked.addListener(callback);
+//     }
+//   }
+// }
