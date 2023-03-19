76e6bb7a04a469dfcc178be312ead6b6c1839258
mallowlabs <mallowlabs@gmail.com>
2023-03-20 08:55:07 +0900

    Add logout button

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,6 +1,20 @@
 import Trello from "trello-web";
 import { trelloAppKey } from "../appkey";
 
+const toggle = () => {
+  chrome.storage.local.get(["token"]).then((result) => {
+    const loginBlock = document.getElementById("login-block");
+    const logoutBlock = document.getElementById("logout-block");
+    if (!result.token) {
+      loginBlock?.classList?.remove("hide");
+      logoutBlock?.classList?.add("hide");
+    } else {
+      loginBlock?.classList?.add("hide");
+      logoutBlock?.classList?.remove("hide");
+    }
+  });
+};
+
 const loginLink = document.getElementById("login-link");
 loginLink?.addEventListener("click", (ev) => {
   console.log("Open options page");
@@ -21,3 +35,12 @@ loginLink?.addEventListener("click", (ev) => {
 
   ev.preventDefault();
 });
+
+const logoutLink = document.getElementById("logout-link");
+logoutLink?.addEventListener("click", () => {
+  chrome.storage.local.remove("token").then(() => {
+    toggle();
+  });
+});
+
+toggle();
