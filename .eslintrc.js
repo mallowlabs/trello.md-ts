9a7f0c4fc978cd17a5a2e1f964f5896ba47676cd
mallowlabs <mallowlabs@gmail.com>
2023-03-19 10:53:49 +0900

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  ignorePatterns: [
+    "extension/js/*.js"
+  ]
+}
