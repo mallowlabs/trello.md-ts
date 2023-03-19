92c2c5db31ff08b8af2a465b59298c15a21a283a
mallowlabs <mallowlabs@gmail.com>
2023-03-19 23:34:48 +0900

    Change Buffer to simple string

--- src/bg/markdown.ts
+++ src/bg/markdown.ts
@@ -2,15 +2,15 @@ import { ListDetailType } from "./listWithCard";
 import { MemberType } from "./member";
 
 export class Markdown {
-  private static h1 = (buffer: Buffer, title: string): void => {
-    buffer.write("# " + title + "\n\n");
+  private static h1 = (buffer: string, title: string): string => {
+    return (buffer += "# " + title + "\n\n");
   };
-  private static h2 = (buffer: Buffer, title: string): void => {
-    buffer.write("## " + title + "\n\n");
+  private static h2 = (buffer: string, title: string): string => {
+    return (buffer += "## " + title + "\n\n");
   };
-  private static quote = (buffer: Buffer, text: string): void => {
+  private static quote = (buffer: string, text: string): string => {
     const s = text.replace(/\n/g, "\n> ");
-    buffer.write("> " + s + "\n\n");
+    return (buffer += "> " + s + "\n\n");
   };
 
   private static avatarUrl = (hash: string): string =>
@@ -36,30 +36,39 @@ export class Markdown {
   };
 
   static format = (xs: ListDetailType[]): string => {
-    const buffer = Buffer.alloc(0);
+    let buffer = "";
     xs.forEach(({ list, cards }) => {
       if (this.accept(list.name)) {
-        this.h1(buffer, list.name);
+        buffer = this.h1(buffer, list.name);
         cards.forEach(({ card, members, actions }) => {
           const cardMembers = members.map(this.avatar).join(" ");
 
-          this.h2(buffer, `[:link:](${card.url}) ${card.name} ${cardMembers}`);
-          this.quote(buffer, card.desc);
+          buffer = this.h2(
+            buffer,
+            `[:link:](${card.url}) ${card.name} ${cardMembers}`
+          );
+          buffer = this.quote(buffer, card.desc);
           actions.forEach(([action, member]) => {
-            this.quote(buffer, "----");
+            buffer = this.quote(buffer, "----");
             if (member) {
-              this.quote(buffer, `${this.avatar(member)} ${action.date}`);
+              buffer = this.quote(
+                buffer,
+                `${this.avatar(member)} ${action.date}`
+              );
             }
             if (action.data.text) {
-              this.quote(buffer, action.data.text);
+              buffer = this.quote(buffer, action.data.text);
             }
             if (action.data.attachment) {
-              this.quote(buffer, this.attachment(action.data.attachment));
+              buffer = this.quote(
+                buffer,
+                this.attachment(action.data.attachment)
+              );
             }
           });
         });
       }
     });
-    return buffer.toString();
+    return buffer;
   };
 }
