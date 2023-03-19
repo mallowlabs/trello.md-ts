5eafc305131e490d36ceb2abca53b84fa48dd921
mallowlabs <mallowlabs@gmail.com>
2023-03-19 09:28:00 +0900

    Remove unused codes

--- src/bg/clipboard.ts
+++ src/bg/clipboard.ts
@@ -1,17 +1,3 @@
-//type document;
-
-// type element = {
-//   value: string;
-//   select: () => void;
-// };
-
-//declare var document: document;
-// declare function get_element_by_id(
-//   doc: Document,
-//   id: string
-// ): element;
-// declare function exec_command(doc: Document, command: string): boolean;
-
 export class Clipboard {
   static write(document: Document, value: string): void {
     const textarea = document.getElementById("ta") as HTMLInputElement; // FIXME
